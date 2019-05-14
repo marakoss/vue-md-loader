c5c31fa0939cab76f6d780b1bdbadbb6a6cdf1c2
Marek Králík <marakoss@gmail.com>
2019-05-14 13:32:30 +0200

    Introduced vue.config with custom webpack chain to support MD and TS

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+	chainWebpack: config => {
+		config.module
+		.rule('md')
+		.test(/\.md$/)
+		.use('vue-loader')
+		.loader('vue-loader')
+		.tap(options => {
+			return options
+		})
+		.end()
+		.use('vue-md-loader')
+		.loader('vue-md-loader')
+		.tap(options => {
+			return options
+		})
+		.end();
+
+		config.module
+		.rule('ts')
+		.test(/\.ts$/)
+		.use('ts-loader')
+		.loader('ts-loader')
+		.tap(options => {
+			return options
+		})
+		.end();
+	}
+}
